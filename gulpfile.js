7426fc8504fb3e498ce956820fc2892ff014b31c
KiselevES <58072524+KiselevES@users.noreply.github.com>
2020-09-29 14:39:45 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
     concat = require('gulp-concat'),
@@ -9,9 +9,11 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     sourceMaps = require('gulp-sourcemaps'),
     stripCssComments = require('gulp-strip-css-comments'),
+    babel = require('gulp-babel'),
+    htmlmin = require('gulp-html-minifier'),
     spritesmith = require('gulp.spritesmith');
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
     return gulp.src('app/scss/**/*.scss')
         .pipe(sourceMaps.init())
         .pipe(sass())
@@ -23,7 +25,8 @@ gulp.task('sass', function () {
         .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('browser-sync', function () {
+
+gulp.task('browser-sync', () => {
     browserSync({
         server: {
             baseDir: 'app'
@@ -32,26 +35,32 @@ gulp.task('browser-sync', function () {
     });
 });
 
-gulp.task('code', function () {
+gulp.task('code', () => {
     return gulp.src('app/*.html')
         .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('myScripts', function () {
-    return gulp.src('app/js/**/*.js')
+// gulp.task('myScripts', () => {
+//     return gulp.src('app/importJS/**/*.js')
+//         .pipe(browserSync.reload({stream: true}))
+// });
+
+gulp.task('scripts', () => {
+    return gulp.src([
+        'app/importJS/main.js'
+    ])
+        .pipe(sourceMaps.init())
+        .pipe(concat('js.min.js'))
+        // .pipe(babel({
+        //     presets: ['@babel/env']
+        // }))
+        // .pipe(uglify())
+        .pipe(sourceMaps.write())
+        .pipe(gulp.dest('app/js'))
         .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('scripts', function () {
-    return gulp.src([
-        'node_modules/jquery/dist/jquery.min.js'
-    ])
-        .pipe(concat('libs.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('app/js'))
-});
-
-gulp.task('spritesmith', async function () {
+gulp.task('spritesmith', () => {
     var spriteData = gulp.src('app/sprites/**/*')
         .pipe(spritesmith({
             imgName: 'sprite.png',
@@ -61,34 +70,42 @@ gulp.task('spritesmith', async function () {
     spriteData.css.pipe(gulp.dest('app/sass/import/'));
 });
 
-gulp.task('clean', async function () {
+//build
+
+gulp.task('clean', async () => {
     return clear.sync('dist')
 });
 
-gulp.task('build', async function () {
+gulp.task('build', async () => {
     gulp.src('app/css/main.min.css')
         .pipe(stripCssComments())
         .pipe(gulp.dest('dist/css'));
 
-    gulp.src('app/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts'));
+    // gulp.src('app/fonts/ **/*')
+    //     .pipe(gulp.dest('dist/fonts'));
 
     gulp.src('app/js/**/*')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
 
     gulp.src('app/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dist'));
 
     gulp.src('app/img/**/*')
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
     gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
     gulp.watch('app/*.html', gulp.parallel('code'));
-    gulp.watch('app/js/**/*.js', gulp.parallel('myScripts'));
+    gulp.watch('app/importJS/**/*.js', gulp.parallel('scripts'));
 });
+
 
 gulp.task('rebuild', gulp.series('clean', 'build'));
 
-gulp.task('default', gulp.parallel('sass', 'scripts', 'myScripts', 'browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
